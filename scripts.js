d578108ec2353e37dd2e7689289fcd435efc0588
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-06-06 05:02:12 +0200

    Add menu behaviour

--- scripts.js
+++ scripts.js
@@ -1,27 +1,23 @@
-function sprawdzZgodnosc() {
-    tekst1 = $("#poleTekstowe1").val();
-    tekst2 = $("#poleTekstowe2").val();
-
-    if (tekst1 == tekst2) {
-        $("#poleTekstowe3").val(' ');
-        $("#poleTekstowe3").val("Tekst jest zgodny");
-    } else {
-        $("#poleTekstowe3").val(' ');
-        $("#poleTekstowe3").val("Tekst nie jest zgodny");
-    }
-}
-
-function licz() {
-    val1 = +$("#poleTekstowe4").val();
-    val2 = +$("#poleTekstowe5").val();
-
-    if (!isNaN(val1) && !isNaN(val2)) {
-        $("#poleTekstowe6").val(' ');
-        $("#poleTekstowe6").val((val1 + val2))
-    }
-}
-
-function DodajParagraf() {
-    element = $("<p></p>").css("backgroundColor", $("#poleTekstowe8").val());
-    $("body").append(element.text($("#poleTekstowe7").val()));
-}
+$(document).ready(function() {
+    $("#logo-button").on("click", function() {
+        location.replace("index.html");
+    });
+    $("#gatunki-button").on("click", function() {
+        location.replace("gatunki.html");
+    });
+    $("#pielegnacja-button").on("click", function() {
+        location.replace("pielegnacja.html");
+    });
+    $("#ciecie-button").on("click", function() {
+        location.replace("ciecie.html");
+    });
+    $("#ciekawostki-button").on("click", function() {
+        location.replace("ciekawostki.html");
+    });
+    $("#quiz-button").on("click", function() {
+        location.replace("quiz.html");
+    });
+    $("#galeria-button").on("click", function() {
+        location.replace("galeria.html");
+    });
+});
